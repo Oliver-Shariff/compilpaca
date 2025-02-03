c709331faebea27be408f2e8a7f9bdd48e4c56f5
Oliver-Shariff <143436031+Oliver-Shariff@users.noreply.github.com>
2025-02-03 14:05:03 -0500

    commit 2: token list added as enum in src/lexer/tokens.ts

---
+++ src/lexer/tokens.ts
@@ -0,0 +1,26 @@
+export enum AlpacaTokens{
+
+	INTEGER = "int",
+	STRING = "string",
+	BOOLEAN = "bool",
+	PRINT = "print",
+	WHILE = "while",
+	IF = "if", 
+	SPACE = " ", 
+	ISEQUAL = "==",
+	ISNOTEQUAL = "!=",
+	FALSE = "false",
+	TRUE = "true",
+	ADD = "+",
+	EOP = "$",
+	LPAREN = "(",
+	RPAREN = ")",
+	LBRACE = "{",
+	RBRACE = "}",
+	SLASH = "/",
+	STAR = "*"
+
+	
+}
+
+
