0880f3d143ea14b5479635f73ea2e61be617a61a
Oliver-Shariff <143436031+Oliver-Shariff@users.noreply.github.com>
2025-03-13 14:03:21 -0400

    Parsing methods completed
    -Rote process to transform derivation rules into parsing functions
    
    -Parser mostly works as expected when given proper input
    
    to do:
    - Comments breaker parser
    - Proper implementation on index.ts: display output, don;t interrupt lex, count errors

--- source/parse.ts
+++ source/parse.ts
@@ -4,7 +4,7 @@ class Parser {
     private tokens: Token[];
     private current: number = 0;
 
-    constructor(tokens: Token[]){
+    constructor(tokens: Token[]) {
         this.tokens = tokens;
     }
 
@@ -76,15 +76,134 @@ class Parser {
         return { type: 'Program', body }
     }
 
-    private parseBlock(): any{
+    private parseBlock(): any {
         this.consume(tokenType.LBRACE, 'Expected { at start of block');
-        //const statements = this.parseStatementList();
+        const statements = this.parseStatementList();
         this.consume(tokenType.RBRACE, 'Expected } at end of block');
-        return {type: 'Block', /*body: statements*/};
-        //^ lets comment that out so we can test it
+        return { type: 'Block', body: statements };
+
     }
 
-}
+    private parseStatementList(): any {
+        const statements = [];
+        while (!this.check(tokenType.RBRACE) && !this.isAtEnd()) {
+            statements.push(this.parseStatement());
+        }
+        //parse statements and push into array
+        return statements;
+    }
+    private parseStatement(): any {
+        //we can see what type of statement we have based on the first token
+        if (this.match(tokenType.KEYWORD)) {
+            switch (this.previous().value) {
+                case 'print':
+                    return this.parsePrintStatement();
+                case 'while':
+                    return this.parseWhileStatement();
+                case 'if':
+                    return this.parseIfStatement();
+                case 'int':
+                case 'string':
+                case 'boolean':
+                    return this.parseVarDecl();
+            }
+        }
+    if (this.match(tokenType.ID)){
+        return this.parseAssignmentStatement();
+    }
+    if (this.check(tokenType.LBRACE)){
+        return this.parseBlock();
+    }
+    throw new Error ('Unexpected token: ' + this.peek().value);
+    }
+
+    private parsePrintStatement(): any{
+        this.consume(tokenType.LPAREN, 'Expected ( after print');
+        const expr = this.parseExpression();
+        this.consume(tokenType.RPAREN, 'Expected ) after expression');
+        return {type: 'PrintStatement', expression: expr};
+    }
+
+    private parseAssignmentStatement(): any {
+        const id = this.previous();
+        this.consume(tokenType.ASSIGN, 'Expected = in assignment');
+        const expr = this.parseExpression();
+        return { type: 'AssignmentStatement', id, expression: expr };
+    }
+
+    private parseVarDecl(): any {
+        const type = this.previous();
+        const id = this.consume(tokenType.ID, 'Expected variable name');
+        return { type: 'VarDecl', varType: type, id };
+    }
+
+    private parseWhileStatement(): any {
+        const condition = this.parseBooleanExpr();
+        const body = this.parseBlock();
+        return { type: 'WhileStatement', condition, body };
+    }
+
+    private parseIfStatement(): any {
+        const condition = this.parseBooleanExpr();
+        const body = this.parseBlock();
+        return { type: 'IfStatement', condition, body };
+    }
+
+    private parseExpression(): any {
+        if (this.match(tokenType.NUMBER)) {
+            return this.parseIntExpr();
+        }
+        if (this.match(tokenType.QUOTE)) {
+            return this.parseStringExpr();
+        }
+        if (this.match(tokenType.BOOL)) {
+            return { type: 'BooleanLiteral', value: this.previous().value };
+        }
+        if (this.match(tokenType.LPAREN)) {
+            return this.parseBooleanExpr();
+        }
+        if (this.match(tokenType.ID)) {
+            return { type: 'Identifier', name: this.previous().value };
+        }
+        throw new Error('Unexpected expression token: ' + this.peek().value);
+    }
+
+    private parseIntExpr(): any {
+        let left = { type: 'Literal', value: this.previous().value };
+        if (this.match(tokenType.INTOP)) {
+            const operator = this.previous();
+            const right = this.parseExpression();
+            return { type: 'BinaryExpression', left, operator, right };
+        }
+        return left;
+    }
+
+    private parseStringExpr(): any {
+        const chars = [];
+        while (!this.match(tokenType.QUOTE)) {
+            if (this.match(tokenType.CHAR) || this.match(tokenType.SPACE)) {
+                chars.push(this.previous().value);
+            } else {
+                throw new Error('Unexpected token in string: ' + this.peek().value);
+            }
+        }
+        return { type: 'StringLiteral', value: chars.join('') };
+    }
+
+    private parseBooleanExpr(): any {
+        if (this.match(tokenType.BOOL)) {
+            return { type: 'BooleanLiteral', value: this.previous().value };
+        }
+        this.consume(tokenType.LPAREN, 'Expected ( in boolean expression');
+        const left = this.parseExpression();
+        const operator = this.consume(tokenType.EQUALITY, 'Expected == or != in boolean expression');
+        const right = this.parseExpression();
+        this.consume(tokenType.RPAREN, 'Expected ) in boolean expression');
+        return { type: 'BooleanExpression', left, operator, right };
+    }
+
+}//class boundary
+
 export function parse(tokens: Token[]): any {
     const parser = new Parser(tokens);
     return parser.parseProgram();
