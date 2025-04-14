d68bd10bcabff1a9215b849b03db64819f5b8f52
Oliver-Shariff <143436031+Oliver-Shariff@users.noreply.github.com>
2025-04-14 13:40:54 -0400

    Print statement subtree
    -Print statement subtree pattern matched using sextractString helper function

--- source/index.ts
+++ source/index.ts
@@ -51,6 +51,7 @@ function handleLexing() {
                         console.log("below should be AST");
                         console.log(ast.toString());
                         console.log(ast)
+                        outputLog += `\n<span class="info">INFO Semantic Analyzer - Abstract Syntax Tree Program ${programCount}:</span>\n`;
                         outputLog += ast.toString() + "\n";
                     } else {
                         console.log("No AST generated")
--- source/semanticAnalyzer.ts
+++ source/semanticAnalyzer.ts
@@ -55,7 +55,25 @@ export function buildAST(cst: Tree): Tree {
                 }
                 break;
             }
+            case "[PrintStatement]": {
+                astTree.addNode("[PrintStatment]", "branch");
 
+                // Locate the expression (3rd child in CST)
+                const exprNode = node.children[2];
+
+                // Handle the expression (expecting a StringExpression)
+                if (exprNode?.name === "[Expression]") {
+                    const stringExpr = exprNode.children[0];
+                    if (stringExpr?.name === "[StringExpression]") {
+                        const charListNode = stringExpr.children[1]; // after opening quote
+                        const strValue = extractString(charListNode); //helper function handles charlist
+                        astTree.addNode(strValue, "leaf");
+                    }
+                }
+
+                astTree.endChildren();
+                break;
+            }
             default:
                 for (const child of node.children) {
                     visit(child, astTree);
@@ -63,6 +81,23 @@ export function buildAST(cst: Tree): Tree {
                 break;
         }
     }
+    function extractString(charListNode: TreeNode | undefined): string {
+        let result = "";
+    
+        let current = charListNode;
+        while (current && current.name === "[CharList]") {
+            const charNode = current.children[0]; // [CHAR]
+            const valueNode = charNode?.children[0]; // actual character
+            if (valueNode?.name) {
+                result += valueNode.name;
+            }
+            // descend into next CharList
+            current = current.children[1]; // nested CharList
+        }
+    
+        return result;
+    }
+    
 
     visit(cst.root, ast);
     return ast;
