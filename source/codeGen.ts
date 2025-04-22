dfac68ce9712ca6ec30bccced8b0054483958870
Oliver-Shariff <143436031+Oliver-Shariff@users.noreply.github.com>
2025-04-22 18:29:58 -0400

    devNotes update

--- source/codeGen.ts
+++ source/codeGen.ts
@@ -4,14 +4,34 @@ import { Scope } from './scopeAnalyzer.js';
 
 class Static {
     temp: number;
-    var: string;
+    id: string;
     address: number;
+
+    constructor(temp: number, id: string, address: number) {
+        this.temp = temp;
+        this.id = id;
+        this.address = address;
+    }
+
+}
+const staticTable: Static[] = []
+
+
+
+function staticEntry() {
+    if (staticTable.length == 0) {
+        const newStatic = new Static()
+
+    }
+    else {
+
+    }
 }
 
 export function generateCode(ast: Tree, scopeTree: Scope): number[] {
-    const staticTable: Static[] = []
     const code: number[] = new Array(256).fill(0x00);
-    let currentIndex = 0;
+    let codeIndex = 0;
+    let staticIndex = 0;
 
     function visit(node: TreeNode): void {
         console.log(`switch on ${node.name}`);
@@ -26,8 +46,13 @@ export function generateCode(ast: Tree, scopeTree: Scope): number[] {
                 console.log(`switch on ${type}`);
                 switch (type) {
                     case "int":
-                        code[currentIndex++] = 0xA9;
-                        code[currentIndex++] = 0x00;
+                        code[codeIndex++] = 0xA9;
+                        code[codeIndex++] = 0x00;
+                        const newStatic = new Static(staticIndex++, node.children[1].name, 0)
+
+                        staticTable[staticIndex] = newStatic
+                        console.log(
+                            staticTable[staticTable.length-1].id);
                         break;
                 }
                 break;
