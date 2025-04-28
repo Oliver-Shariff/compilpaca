eb7eadc70c8972faa46ff2fef89af2ee0bd8507c
Oliver-Shariff <143436031+Oliver-Shariff@users.noreply.github.com>
2025-04-28 05:59:32 -0400

    dev notes
    Should we make changes to semanticAnalyzer? see notes

--- source/codeGen.ts
+++ source/codeGen.ts
@@ -8,14 +8,16 @@ class Static {
     scope: number;
     size: number; //not using length to avoid static.length vs static[].length confusion
     locations: number[] = []
-
-    constructor(id: string, scope: number, size: number) {
+    value: number;
+    
+    constructor(id: string, scope: number, size: number, value: number) {
         this.id = id;
         this.scope = scope;
         this.size = size;
         this.name = `${id}@${scope}`; //for clean debug
+        this.value = value;
     }
-
+    
 }
 
 function staticEntry(id: string, scope: number, size: number) {
@@ -24,17 +26,27 @@ function staticEntry(id: string, scope: number, size: number) {
 
 const staticTable: Static[] = []
 
-export function generateCode(ast: Tree, scopeTree: Scope): number[] {
+
+export function generateCode(ast: Tree, rootScope: Scope): number[] {
     const code: number[] = new Array(256).fill(0x00);
     let codeIndex = 0;
     let staticIndex = 0;
-
+    let currentScope = rootScope;
+    
+    function gotoChild(){
+        //which child?
+    }
+    function gotoParent(){
+        
+    }
+    
     function visit(node: TreeNode): void {
         console.log(`switch on ${node.name}`);
         switch (node.name) {
 
             case "[Program]":
             case "[Block]":
+                //I want to enter and exit scope here like in scopeAnalyzer but I'll need to handle the base case
                 for (const child of node.children) visit(child);
                 break;
             case "[VariableDeclaration]": {
