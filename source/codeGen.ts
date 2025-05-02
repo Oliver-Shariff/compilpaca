3cabfbfeacae0587bc9d4768aa10c204927c794f
Oliver-Shariff <143436031+Oliver-Shariff@users.noreply.github.com>
2025-05-02 13:48:47 -0400

    Assignment statement, string handling, and expression handling
    -generateExpression() used to recuresively handle expressions (addition) considerations made for left and right side of expression as well as adding variables in chained additon
    -static object further altered to handle strings and storage in heap memory
    -string data is held in static object and heap is written after stack is completed to save memory
    -backpatching of temp memory location happens once heap is written

--- source/codeGen.ts
+++ source/codeGen.ts
@@ -7,9 +7,10 @@ class Static {
     id: string; //variable name
     scope: number;
     size: number; //not using length to avoid static.length vs static[].length confusion
-    locations: number[] = [] //all the target locations in code array that need to be overwritten
-    value: number; // the actual mem location of the variable
-    
+    stackTargets: number[] = [] //all the target locations in code array that need to be overwritten
+    heapLocation: number = -1; // the actual mem location of the variable
+    stringValue?: string;
+
     constructor(id: string, scope: number, size?: number) {
         this.id = id;
         this.scope = scope;
@@ -17,29 +18,82 @@ class Static {
         this.name = `${id}@${scope}`; //for clean debug
 
     }
-    
+
 }
-function addLocation(name: string, location: number){
-    for (let i = 0; i < staticTable.length; i++ ){
-        if (staticTable[i].name == name){
-            staticTable[i].locations.push(location);
+const staticTable: Static[] = []
+
+
+
+function addLocation(name: string, location: number) {
+    for (let i = 0; i < staticTable.length; i++) {
+        if (staticTable[i].name == name) {
+            staticTable[i].stackTargets.push(location);
         }
     }
 
-
 }
-
-const staticTable: Static[] = []
-
 
 export function generateCode(ast: Tree): number[] {
     const code: number[] = new Array(256).fill(0x00);
     let codeIndex = 0;
     let staticIndex = 0;
-    
+
+    function generateExpression(node: TreeNode): void {
+        if (
+            node.name === "[Addition]") {
+            generateExpression(node.children[0]);
+
+            const right = node.children[1]; //right side of the expression
+
+            if (/^\d+$/.test(right.name)) { //number literal
+                code[codeIndex++] = 0x6D; // ADC #val
+                code[codeIndex++] = parseInt(right.name);
+            } else if (/^[a-z]$/.test(right.name)) { //variable
+                const ref = staticTable.find(e => e.name === `${right.name}@${node.scopeId}`);
+                if (!ref) throw new Error(`Var '${right.name}' not found`);
+                code[codeIndex++] = 0x6D;
+                addLocation(ref.name, codeIndex);
+                code[codeIndex++] = 0x00;
+                code[codeIndex++] = 0x00;
+            } else if (right.name === "[Addition]") { //nested addition
+                const tempVar = `_tmp${codeIndex}`;
+                const temp = new Static(tempVar, 0);
+                temp.heapLocation = codeIndex++;
+                staticTable.push(temp);
+
+                code[codeIndex++] = 0x8D;
+                addLocation(temp.name, codeIndex);
+                code[codeIndex++] = 0x00;
+                code[codeIndex++] = 0x00;
+
+                generateExpression(right);
+
+                code[codeIndex++] = 0x6D;
+                addLocation(temp.name, codeIndex);
+                code[codeIndex++] = 0x00;
+                code[codeIndex++] = 0x00;
+            } else {
+                throw new Error(`Unhandled RHS: ${right.name}`);
+            }
+        } else if (/^\d+$/.test(node.name)) {
+            code[codeIndex++] = 0xA9;
+            code[codeIndex++] = parseInt(node.name);
+        } else if (/^[a-z]$/.test(node.name)) {
+            const ref = staticTable.find(e => e.name === `${node.name}@${node.scopeId}`);
+            if (!ref) throw new Error(`Var '${node.name}' not found`);
+            code[codeIndex++] = 0xAD;
+            addLocation(ref.name, codeIndex);
+            code[codeIndex++] = 0x00;
+            code[codeIndex++] = 0x00;
+        } else {
+            throw new Error(`Unhandled expr: ${node.name}`);
+        }
+    }
+
+
     function visit(node: TreeNode): void {
-//        console.log(`switch on ${node.name}`);
-        
+        //        console.log(`switch on ${node.name}`);
+
         switch (node.name) {
 
             case "[Program]":
@@ -57,29 +111,71 @@ export function generateCode(ast: Tree): number[] {
                         code[codeIndex++] = 0xA9;//load acc with const
                         code[codeIndex++] = 0x00;//initialize to zero
                         code[codeIndex++] = 0x8D; //store acc in memory
-                        const newStatic = new Static(id,scope, 1); //create the static entry since this is a var decl
+                        const newStatic = new Static(id, scope, 1); //create the static entry since this is a var decl
                         staticTable[staticIndex++] = newStatic;//add the static object to the static table
                         let name = `${id}@${scope}`
-                        addLocation(name,codeIndex++)//update the locations this object holds
+                        addLocation(name, codeIndex++)//update the locations this object holds
                         code[codeIndex++] = 0x00;
                         break;
                     case "string":
-                        const stringStatic = new Static(id,scope)
+                        const stringStatic = new Static(id, scope)
                         staticTable[staticIndex++] = stringStatic;
                         break;
                 }
                 break;
             }
-            default:
+            case "[AssignmentStatement]": {
+                const idNode = node.children[0];
+                const valueNode = node.children[1];
+                const name = `${idNode.name}@${node.scopeId}`;
+                const target = staticTable.find(entry => entry.name === name);
+                if (!target) throw new Error(`No static for ${name}`);
+
+                if (/^".*"$/.test(valueNode.name)) { // string
+                    const raw = valueNode.name.slice(1, -1);
+                    target.stringValue = raw;
+
+                    code[codeIndex++] = 0xA9;
+                    addLocation(target.name, codeIndex);
+                    code[codeIndex++] = 0x00;
+
+                    code[codeIndex++] = 0x8D;
+                    addLocation(target.name, codeIndex);
+                    code[codeIndex++] = 0x00;
+                    code[codeIndex++] = 0x00;
+                } else {
+                    generateExpression(valueNode);
+                    code[codeIndex++] = 0x8D;
+                    addLocation(target.name, codeIndex);
+                    code[codeIndex++] = 0x00;
+                    code[codeIndex++] = 0x00;
+                }
+
                 break;
+            }
+            
+
+
+            default:
+                for (const child of node.children) visit(child);
+                break;
+
         }
     }
-    function fillStatic(){
-        for(let i = 0; i < staticTable.length; i++){//for every entry in the static table
-            //I need to add logic here to handle strings which are longer than 1 location
-            staticTable[i].value = codeIndex++;// after visit is over this will be the first free location
-            for(let j = 0; j < staticTable[i].locations.length; j++){//at each target location saved
-                code[staticTable[i].locations[j]] = staticTable[i].value; //write the value at the target location
+    function fillStatic() {
+        for (const entry of staticTable) {
+            if (entry.stringValue !== undefined) {
+                entry.heapLocation = codeIndex;
+                for (const char of entry.stringValue) {
+                    code[codeIndex++] = char.charCodeAt(0);
+                }
+                code[codeIndex++] = 0x00; // null terminator
+            } else if (entry.heapLocation === -1) {
+                entry.heapLocation = codeIndex++;
+            }
+
+            for (const loc of entry.stackTargets) {
+                code[loc] = entry.heapLocation;
             }
         }
     }
@@ -87,6 +183,7 @@ export function generateCode(ast: Tree): number[] {
     if (ast.root) {
         visit(ast.root);
         fillStatic();
+        code[codeIndex++] = 0x00; //BRK
     }
 
 
